11bacdfe53def94af6cb0539a4282e51ec03608f
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2022-12-08 14:59:47 -0300

    att

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error("Esse cep não existe!")
-        }else
-        console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log("Processamento concluído!"));
-
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
